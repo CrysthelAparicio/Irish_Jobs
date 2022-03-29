106771ce78e7a9c349d6736b5eabb93ab032dee5
CrysthelAparicio <crysapa13@gmail.com>
2022-03-29 02:13:40 -0400

    Login

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -12,8 +12,20 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://www.irishjobs.ie/login");
-
-
+        //*[@id="app"]/div[2]/div/div[2]/button[1]
+        /////*[@id="app"]/div[2]/div/div[2]/button[2]
+        WebElement CookiesButton = driver.findElement(By.xpath("*[@id=\"app\"]/div[2]/div/div[2]/button[2]"));
+        CookiesButton.click();
+        //Login
+        //*[@id="Email"]
+        WebElement inputName = driver.findElement(By.id("Email"));
+        inputName.sendKeys("apariciocrysthel@gmail.com");
+        ////*[@id="Password"]
+        WebElement inputPass = driver.findElement(By.id("Password"));
+        inputPass.sendKeys("Guitarra3151");
+        ///* //*[@id="loginButton"]
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+        loginButton.click();
 
     }
 
