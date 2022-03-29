91df5c173b81dabc818d3fb1c5ecd7e4041e16e7
CrysthelAparicio <crysapa13@gmail.com>
2022-03-29 06:42:26 -0400

    My Account

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,31 +1,42 @@
 package Base;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Driver;
+
 public class BaseTest {
-    private WebDriver driver;
+    private WebDriver Webdriver;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.irishjobs.ie/login");
+        Webdriver = new ChromeDriver();
+        Webdriver.get("https://www.irishjobs.ie/login");
         //*[@id="app"]/div[2]/div/div[2]/button[1]
         /////*[@id="app"]/div[2]/div/div[2]/button[2]
-        WebElement CookiesButton = driver.findElement(By.xpath("*[@id=\"app\"]/div[2]/div/div[2]/button[2]"));
-        CookiesButton.click();
+
+        System.out.println("Login");
+     //*   WebElement CookiesButton = driver.findElement(By.xpath("*[@id=\"app\"]/div[2]/div/div[2]/button[2]"));
+       //** CookiesButton.click();
         //Login
         //*[@id="Email"]
-        WebElement inputName = driver.findElement(By.id("Email"));
+        WebElement inputName = Webdriver.findElement(By.id("Email"));
         inputName.sendKeys("apariciocrysthel@gmail.com");
+        inputName.sendKeys(Keys.ENTER);
         ////*[@id="Password"]
-        WebElement inputPass = driver.findElement(By.id("Password"));
+        WebElement inputPass = Webdriver.findElement(By.id("Password"));
         inputPass.sendKeys("Guitarra3151");
         ///* //*[@id="loginButton"]
-        WebElement loginButton = driver.findElement(By.id("loginButton"));
+        inputPass.sendKeys(Keys.ENTER);
+
+        WebElement loginButton = Webdriver.findElement(By.linkText("loginButton"));
         loginButton.click();
+
+
+
 
     }
 
